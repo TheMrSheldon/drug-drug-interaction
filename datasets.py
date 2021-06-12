f2988af944b342a2d8322769cc6449f8677bb215
Tim Hagen <timhagenmail@googlemail.com>
2021-06-12 19:28:54 +0200

    Refactored the training so far

--- datasets.py
+++ datasets.py
@@ -1,5 +1,6 @@
 from torch_geometric.datasets import Planetoid
 from ogb.linkproppred import PygLinkPropPredDataset
+import torch
 import torch_geometric.transforms as T
 
 class Datasets:
@@ -9,8 +10,15 @@ class Datasets:
     DrugDrugInteraction = 'ogbl-ddi';
     ProteinProteinAssociation = 'ogbl-ppa';
 
-def load_dataset(path, name):
+def load_dataset(path, name, device):
     if name.startswith('ogbl-'):
-        return PygLinkPropPredDataset(name = name, transform=T.ToSparseTensor())
+        dataset = PygLinkPropPredDataset(name = name, transform=T.ToSparseTensor())
+        split_edge = dataset.get_edge_split()
+        data = dataset[0]
+        idx = torch.randperm(split_edge['train']['edge'].size(0))
+        idx = idx[:split_edge['valid']['edge'].size(0)]
+        split_edge['eval_train'] = {'edge': split_edge['train']['edge'][idx]}
+        adj_t = data.adj_t.to(device)
+        return (data.num_nodes, adj_t, split_edge)
     else:
         return Planetoid(path, name = name, split='public')
--- main.py
+++ main.py
@@ -1,41 +1,53 @@
+from torch.nn.functional import embedding
 from datasets import load_dataset, Datasets
 import gnn
 import torch.nn
 import torch.optim
-from ogb.linkproppred import Evaluator
+from ogb.linkproppred import Evaluator, evaluate
 
-# Reproduce
+def init_sage(hidden_channels, num_datanodes, num_layers, dropout, device):
+    model = gnn.SAGE(hidden_channels, hidden_channels, hidden_channels, num_layers, dropout).to(device)
+    embedding = torch.nn.Embedding(num_datanodes, hidden_channels).to(device)
+    predictor = gnn.LinkPredictor(hidden_channels, hidden_channels, 1, num_layers, dropout).to(device)
+    evaluator = Evaluator(Datasets.DrugDrugInteraction)
+    return (model, embedding, predictor, evaluator)
 
+def train(model, embedding, predictor, num_epochs, learn_rate, adj_t, split_edge, batch_size):
+    # Reset parameters
+    torch.nn.init.xavier_uniform_(embedding.weight)
+    model.reset_parameters()
+    predictor.reset_parameters()
+    # Init the optimizer
+    optimizer = torch.optim.Adam(list(model.parameters()) + list(embedding.parameters()) + list(predictor.parameters()), lr=learn_rate)
+    # Train for multiple epochs
+    for epoch in range(num_epochs):
+        loss = gnn.train(model, predictor, embedding.weight, adj_t, split_edge, optimizer, batch_size)
+        print(loss)
 
+def init_train_eval_sage(dataset, num_epochs, batch_size, device):
+    (num_datanodes, adj_t, split_edge) = load_dataset("./datasets/", dataset, device)
 
-# New Data
+    (model, embedding, predictor, evaluator) = init_sage(hidden_channels=256, num_datanodes=num_datanodes, num_layers=2, dropout=0.5, device=device)
+    train(model, embedding, predictor, num_epochs=num_epochs, learn_rate=0.005, adj_t=adj_t, split_edge=split_edge, batch_size=batch_size)
+    # TODO: evaluate the trained model
+
+# Check if training on GPU is possible
+print(f'Cuda available: {torch.cuda.is_available()}')
 device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-dataset = load_dataset("./datasets/", Datasets.DrugDrugInteraction)
-split_edge = dataset.get_edge_split()
-data = dataset[0]
-adj_t = data.adj_t.to(device)
-idx = torch.randperm(split_edge['train']['edge'].size(0))
-idx = idx[:split_edge['valid']['edge'].size(0)]
-split_edge['eval_train'] = {'edge': split_edge['train']['edge'][idx]}
+######################################################################################################################
+# TASKS                                                                                                              #
+######################################################################################################################
+### Reproduce
+# Does not yet work
+#init_train_eval_sage(Datasets.CiteSeer, 200, 64*1024, device)
+#init_train_eval_sage(Datasets.PubMed, 200, 64*1024, device)
+#init_train_eval_sage(Datasets.Cora, 200, 64*1024, device)
 
-hidden_channels = 256
-num_layers = 2
-dropout = 0.5
-learn_rate = 0.005
-batch_size = 64 * 1024
-model = gnn.SAGE(hidden_channels, hidden_channels, hidden_channels, num_layers, dropout).to(device)
-emb = torch.nn.Embedding(data.num_nodes, hidden_channels).to(device)
-predictor = gnn.LinkPredictor(hidden_channels, hidden_channels, 1, num_layers, dropout).to(device)
-evaluator = Evaluator(Datasets.DrugDrugInteraction)
+### New Data
+init_train_eval_sage(Datasets.DrugDrugInteraction, 200, 64*1024, device)
+# Does not yet work
+init_train_eval_sage(Datasets.ProteinProteinAssociation, 200, 64*1024, device)
 
-torch.nn.init.xavier_uniform_(emb.weight)
-model.reset_parameters()
-predictor.reset_parameters()
-optimizer = torch.optim.Adam(list(model.parameters()) + list(emb.parameters()) + list(predictor.parameters()), lr=learn_rate)
-for epoch in range(200):
-    loss = gnn.train(model, predictor, emb.weight, adj_t, split_edge, optimizer, batch_size)
-    print(loss)
-
-# Hyperparams Check
+### Hyperparams Check
