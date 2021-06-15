a0e01069e498dad0f06c93b1e5c2b98e3588a5b9
Tim Hagen <timhagenmail@googlemail.com>
2021-06-15 20:12:47 +0200

    preliminary implementation of the evaluation

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def init_sage(hidden_channels, num_datanodes, num_layers, dropout, device):
     evaluator = Evaluator(Datasets.DrugDrugInteraction)#TODO: maybe init the evaluator to the specified metric here (would also need to add the metric to the function parameters)
     return (model, embedding, predictor, evaluator)
 
-def train(model, embedding, predictor, metric, num_epochs, learn_rate, graph, split_edge, batch_size):
+def train(model, embedding, predictor, evaluator, num_epochs, learn_rate, graph, split_edge, batch_size):
     #TODO: use metric (maybe not needed here?)
 
     # Reset parameters
@@ -30,7 +30,9 @@ def train(model, embedding, predictor, metric, num_epochs, learn_rate, graph, sp
     # Train for multiple epochs
     for epoch in range(num_epochs):
         loss = gnn.train(model, predictor, embedding.weight, graph.adj_t.to(device), split_edge, optimizer, batch_size)
-        print(loss)
+        if (epoch+1) % 10 == 0:
+            results = gnn.test(model, predictor, embedding.weight, graph.adj_t, split_edge, evaluator, batch_size)
+            print(results)
 
 def init_train_eval_sage(dataset, metric, num_epochs, batch_size, device):
     #TODO: use metric (maybe part of evaluator)
@@ -38,7 +40,7 @@ def init_train_eval_sage(dataset, metric, num_epochs, batch_size, device):
     (graph, split_edge) = load_dataset("./datasets/", dataset, device)
 
     (model, embedding, predictor, evaluator) = init_sage(hidden_channels=256, num_datanodes=graph.num_nodes, num_layers=2, dropout=0.5, device=device)
-    train(model, embedding, predictor, metric, num_epochs=num_epochs, learn_rate=0.005, graph=graph, split_edge=split_edge, batch_size=batch_size)
+    train(model, embedding, predictor, evaluator, num_epochs=num_epochs, learn_rate=0.005, graph=graph, split_edge=split_edge, batch_size=batch_size)
     # TODO: evaluate the trained model
 
 # Check if training on GPU is possible
