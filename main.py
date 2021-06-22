3c41d30f7bbb8b4d0c8944225a13ed05cf4a40a3
Sheldon <timhagenmail@googlemail.com>
2021-06-22 13:16:39 +0200

    implemented the dataloader to use multiple workers... lets see how that works out

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def train(model, embedding, predictor, evaluator, num_epochs, learn_rate, graph,
 def init_train_eval(embedding_model, dataset, metric, num_epochs, batch_size, device):
     print(f'==Training dataset: {dataset}==')
     (graph, split_edge) = load_dataset("./datasets/", dataset, device)
-    (model, embedding, predictor) = init_sage(hidden_channels=256, num_datanodes=graph.num_nodes, num_layers=2, dropout=0.5, device=device)
+    (model, embedding, predictor) = init_sage(hidden_channels=20, num_datanodes=graph.num_nodes, num_layers=2, dropout=0.5, device=device)
     evaluator = evaluator = create_evaluator(dataset, metric)
     train(model, embedding, predictor, evaluator, num_epochs=num_epochs, learn_rate=0.005, graph=graph, split_edge=split_edge, batch_size=batch_size)
     print(gnn.test(model, predictor, embedding.weight, graph.adj_t, split_edge, evaluator, batch_size))
@@ -45,40 +45,41 @@ def run(embedding_models, datasets_and_metrics, num_epochs, device):
         for dataset, metrics in datasets_and_metrics:
             init_train_eval(model, dataset, metrics, num_epochs, batch_size, device)
 
-# Check if training on GPU is possible
-print(f'Cuda available: {torch.cuda.is_available()}')
-device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
-print(f'Training device: {device}')
+if __name__ == '__main__':
+    # Check if training on GPU is possible
+    print(f'Cuda available: {torch.cuda.is_available()}')
+    device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f'Training device: {device}')
 
-######################################################################################################################
-# TASKS                                                                                                              #
-######################################################################################################################
-Datasets1 = [(Datasets.CiteSeer, Metrics.MRR), (Datasets.PubMed, Metrics.MRR), (Datasets.Cora, Metrics.MRR)]
-Datasets2 = [(Datasets.DrugDrugInteraction, Metrics.HitsAt(20)), (Datasets.ProteinProteinAssociation, Metrics.HitsAt(20))]
-### Reproduce
-def task_reproduce():
-    run([EmbeddingModels.GraphSAGE], Datasets1, 200, device)
+    ######################################################################################################################
+    # TASKS                                                                                                              #
+    ######################################################################################################################
+    Datasets1 = [(Datasets.CiteSeer, Metrics.MRR), (Datasets.PubMed, Metrics.MRR), (Datasets.Cora, Metrics.MRR)]
+    Datasets2 = [(Datasets.ProteinProteinAssociation, Metrics.HitsAt(20))]#[(Datasets.DrugDrugInteraction, Metrics.HitsAt(20)), (Datasets.ProteinProteinAssociation, Metrics.HitsAt(20))]
+    ### Reproduce
+    def task_reproduce():
+        run([EmbeddingModels.GraphSAGE], Datasets1, 200, device)
 
-### New Data
-def task_new_data():
-    run([EmbeddingModels.GraphSAGE], Datasets2, 200, device)
+    ### New Data
+    def task_new_data():
+        run([EmbeddingModels.GraphSAGE], Datasets2, 200, device)
 
-### Hyperparams Check
-def task_hyperparams_check():
-    #### Different Number of Neighbors
-    #TODO
-    #### Different Number of Epochs
-    run([EmbeddingModels.GraphSAGE], Datasets1, 100, device)
-    run([EmbeddingModels.GraphSAGE], Datasets1, 300, device)
+    ### Hyperparams Check
+    def task_hyperparams_check():
+        #### Different Number of Neighbors
+        #TODO
+        #### Different Number of Epochs
+        run([EmbeddingModels.GraphSAGE], Datasets1, 100, device)
+        run([EmbeddingModels.GraphSAGE], Datasets1, 300, device)
 
-    #### Different Depth
-    #TODO
+        #### Different Depth
+        #TODO
 
-    ### New Algorithm Variant
-    #TODO
+        ### New Algorithm Variant
+        #TODO
 
-### Ablation Study
-def task_ablation_study():
-    run([EmbeddingModels.DeepWalk, EmbeddingModels.Node2Vec], Datasets1+Datasets2, 200, device)
+    ### Ablation Study
+    def task_ablation_study():
+        run([EmbeddingModels.DeepWalk, EmbeddingModels.Node2Vec], Datasets1+Datasets2, 200, device)
 
-task_new_data()
+    task_new_data()
